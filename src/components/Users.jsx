f829d2918edddb730939bfd10c25c280ae9bea82
XXXRAINIER09 <rainiersison123@gmail.com>
2023-11-29 14:50:03 +0800

    change text version to 1.86

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,7 @@
 function Users() {
   return (
     <section className="p-8 md:px-15">
-      <div className=" px-2 md:px-40">
+      <div className="px-2 md:px-40">
         <ul className="p-7 border-[1px] border-[#212121] rounded-lg max-w-full grid grid-cols-2 md:grid-cols-4 text-[#bebebe] justify-around items-center text-center gap-7">
           <li>
             <div className=" text-white font-semibold text-sm md:text-[1.1rem] leading-relaxed">
@@ -21,7 +21,7 @@ function Users() {
           </li>
           <li>
             <div className="text-white font-semibold text-sm md:text-[1.1rem] leading-relaxed">
-              1.84
+              1.86
             </div>
             <div>
               <p className="text-xs">Version</p>
