6612c72f789a651a8f810e8b35acfffd534c06da
XXXRAINIER09 <rainiersison123@gmail.com>
2024-04-26 16:58:26 +0800

    fix decimal in num version

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-export const ApkVersion = (1.1).toFixed(2);
+export const ApkVersion = (1.1).toFixed(3);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
